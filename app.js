1c0dd46cd7fcbc49955247ee4ed3278bdc94d597
Frank Blom <frankblom@MBP-van-Frank-3.home>
2019-01-02 22:00:49 -0100

    4

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+//var htmlRouter  = require('./routes/html');
 
 var app = express();
 
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//app.use('/html', htmlRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +42,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
